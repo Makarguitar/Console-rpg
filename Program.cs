ee96d0dfd85c6989176f9d09eabe033b314c3477
makar <makmakhorin@hotmail.com>
2025-02-17 19:51:07 +0400

    added random, add finish of day,, add cycle for days, add hour,

--- Program.cs
+++ Program.cs
@@ -6,34 +6,46 @@
         {
             Console.WriteLine("Type in your nume");
             string userName = Console.ReadLine();
+            Random r = new Random();
 
-            for (int i = 0; i < 10; i++)
+            for (int day = 1; day < 6; day++)
             {
-                Console.WriteLine("1 - walk  2 - talk  3 - rest  4 - finish the day");
-                Console.WriteLine("Choose an action");
-                int numOfAction = int.Parse(Console.ReadLine());
-                if (numOfAction == 1)
+                for (int i = 0; i < 5; i++)
                 {
-                    Console.WriteLine($"{userName} went walking");
+                    Console.WriteLine("1 - walk  2 - talk  3 - rest  4 - finish the day");
+                    Console.WriteLine("Choose an action");
+                    int numOfAction = int.Parse(Console.ReadLine());
+                    if (numOfAction == 1)
+                    {
+                        Console.WriteLine($"{userName} went walking");
+                    }
+                    else if (numOfAction == 2)
+                    {
+                        Console.WriteLine($"{userName} started to talk");
+                    }
+                    else if (numOfAction == 3)
+                    {
+                        int hour = r.Next(1, 6);
+                        Console.WriteLine($"{userName} made a camp and went to a rest for {hour}h");
+                    }
+                    else if (numOfAction == 4)
+                    {
+                        Console.WriteLine($"{userName} made a camp and went to sleep");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{userName} doesnt know how to do this action");
+                    }
                 }
-                else if (numOfAction == 2)
+                Console.WriteLine($"{userName} has ended the day: {day} \n");
+                Console.WriteLine("to continue the game please press enter, to stop the game please type 0");
+                if (Console.ReadLine() == "0")
                 {
-                    Console.WriteLine($"{userName} started to talk");
+                    break;
                 }
-                else if (numOfAction == 3)
-                {
-                    Console.WriteLine($"{userName} made a camp and went to a rest");
-                }
-                else if (numOfAction == 4)
-                {
-                    Console.WriteLine($"{userName} made a camp and went to sleep");
-                }
-                else
-                {
-                    Console.WriteLine($"{userName} doesnt know how to do this action");
-                }
-                
             }
+            
 
                        
         }
