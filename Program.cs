985627c49b8af9e1f7b1d4ebb0c13896a7717088
makar <makmakhorin@hotmail.com>
2025-03-13 16:08:16 +0400

    made classes, made functions, made cool box around text

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System.ComponentModel;
+using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Console_rpg
 {
@@ -21,21 +24,37 @@ namespace Console_rpg
 
     internal class Program
     {
+        static string[] typeNames = { "nobody knows", "Bowman", "Warrior", "Magician" };
+        static string[] bonusNames = { "No bonus", "More Hp", "More Luck", "More Damage" };
+        static int choice;
         static void Main(string[] args)
         {
-            string[] typeNames = { "nobody knows", "Bowman", "Warrior", "Magician" };
-            string[] bonusNames = { "No bonus", "More Hp", "More Luck", "More Damage" };
-            Console.WriteLine("Type in your nume");
-            string userName = Console.ReadLine();
-            Random r = new Random();
-            TypeOfCharacter type = TypeOfCharacter.None;
-            TypeOfBonus bonus = TypeOfBonus.None;
-            int choice;
-            int hp = 100;
-            int luck = 30;
-            int damage = 30;
+            PrintTextWithBorder("Hello, my name is bozmh, you will play the hardest game in the wurld and you will rage quite");
+            Character user = new Character();
+            CustomizeCharacter(user);
 
-            while (type == TypeOfCharacter.None)
+            GameLoop(user); 
+                       
+        }
+
+        static void CustomizeCharacter(Character character)
+        {
+            Console.WriteLine("Type in your nume");
+
+            character.name = Console.ReadLine();
+            ChooseType(character);
+            ChooseBonus(character);
+
+
+            Console.WriteLine($"You chose the {typeNames[(int)character.type]}");
+
+            Console.WriteLine($"You chose banus: {bonusNames[(int)character.bonus]}");
+            Console.WriteLine($"Your attributes: hp = {character.hp}, luck = {character.luck}, damage = {character.damage}");
+        }
+
+        static void ChooseType(Character user)
+        {
+            while (user.type == TypeOfCharacter.None)
             {
                 Console.WriteLine("Choose a churucter");
                 Console.WriteLine($"1 - {typeNames[(int)TypeOfCharacter.BowMan]}, 2 - {typeNames[(int)TypeOfCharacter.Warrior]}, 3 - {typeNames[(int)TypeOfCharacter.Magician]}");
@@ -44,22 +63,24 @@ namespace Console_rpg
                 switch (choice)
                 {
                     case 1:
-                        type = TypeOfCharacter.BowMan;
+                        user.type = TypeOfCharacter.BowMan;
                         break;
                     case 2:
-                        type = TypeOfCharacter.Warrior;
+                        user.type = TypeOfCharacter.Warrior;
                         break;
                     case 3:
-                        type = TypeOfCharacter.Magician;
+                        user.type = TypeOfCharacter.Magician;
                         break;
                     default:
                         Console.WriteLine("this is not a type");
                         break;
                 }
             }
-            Console.WriteLine($"You chose the {typeNames[(int)type]}");
+        }
 
-            while (bonus == TypeOfBonus.None)
+        static void ChooseBonus(Character user)
+        {
+            while (user.bonus == TypeOfBonus.None)
             {
                 Console.WriteLine("Choose a banus");
                 Console.WriteLine($"1 - {bonusNames[(int)TypeOfBonus.Hp]}, 2 - {bonusNames[(int)TypeOfBonus.Luck]}, 3 - {bonusNames[(int)TypeOfBonus.Damage]}");
@@ -68,27 +89,39 @@ namespace Console_rpg
                 switch (choice)
                 {
                     case 1:
-                        bonus = TypeOfBonus.Hp;
-                        hp *= 2;
+                        user.bonus = TypeOfBonus.Hp;
+                        user.hp *= 2;
                         break;
                     case 2:
-                        bonus = TypeOfBonus.Luck;
-                        luck *= 2;
+                        user.bonus = TypeOfBonus.Luck;
+                        user.luck *= 2;
                         break;
                     case 3:
-                        bonus = TypeOfBonus.Damage;
-                        damage *= 2;
+                        user.bonus = TypeOfBonus.Damage;
+                        user.damage *= 2;
                         break;
                     default:
                         Console.WriteLine("this is not a bonus");
                         break;
                 }
-                Console.WriteLine($"You chose banus: {bonusNames[(int)bonus]}");
-                Console.WriteLine($"Your attributes: hp = {hp}, luck = {luck}, damage = {damage}");
-
             }
+        } 
 
+        static void PrintTextWithBorder(string text)
+        {
+            string border = "+";
+            string middle = $"| {text} |";
+            for (int i = 0; i < text.Length + 2; i++)
+            {
+                border += "-";
+            }
+            border += "+";
+            Console.WriteLine($"{border}\n{middle}\n{border}");
+        }
 
+        static void GameLoop(Character user)
+        {
+            Random r = new Random();
             for (int day = 1; day < 6; day++)
             {
                 for (int i = 0; i < 5; i++)
@@ -99,53 +132,58 @@ namespace Console_rpg
                     switch (numOfAction)
                     {
                         case 1:
-                            Console.WriteLine($"{userName} went somewhere");
+                            Console.WriteLine($"{user.name} went somewhere");
                             string[] directions = { "west", "east", "south", "north" };
                             for (int j = 0; j < 3; j++)
                             {
                                 int km = r.Next(1, 10);
                                 int direction = r.Next(0, directions.Length);
-                                Console.WriteLine($"{userName} went walking for {km} km to {directions[direction]}");
+                                Console.WriteLine($"{user.name} went walking for {km} km to {directions[direction]}");
                             }
                             break;
                         case 2:
-                            Console.WriteLine($"{userName} started to talk");
+                            Console.WriteLine($"{user.name} started to talk");
                             /*
                             int hours = 1;
                             while (hours <= 3)
                             {
-                                Console.WriteLine($"{userName} talked {hours}h");
+                                Console.WriteLine($"{user.name} talked {hours}h");
                                 hours++;
                             }
                             */
                             for (int hours = 1; hours <= 3; hours++)
                             {
-                                Console.WriteLine($"{userName} talked {hours}h");
+                                Console.WriteLine($"{user.name} talked {hours}h");
                             }
                             break;
                         case 3:
                             int hour = r.Next(1, 6);
-                            Console.WriteLine($"{userName} made a camp and went to a rest for {hour}h");
+                            Console.WriteLine($"{user.name} made a camp and went to a rest for {hour}h");
                             break;
                         case 4:
-                            Console.WriteLine($"{userName} made a camp and went to sleep");
+                            Console.WriteLine($"{user.name} made a camp and went to sleep");
                             i = 5;
                             break;
                         default:
-                            Console.WriteLine($"{userName} doesnt know how to do this action");
+                            Console.WriteLine($"{user.name} doesnt know how to do this action");
                             break;
                     }
                 }
-                Console.WriteLine($"{userName} has ended the day: {day} \n");
+                Console.WriteLine($"{user.name} has ended the day: {day} \n");
                 Console.WriteLine("to continue the game please press enter, to stop the game please type 0");
                 if (Console.ReadLine() == "0")
                 {
                     break;
                 }
             }
-            
-
-                       
         }
+    }
+
+    class Character
+    {
+        public string name;
+        public int hp = 100, damage = 30, luck = 30;
+        public TypeOfCharacter type = TypeOfCharacter.None;
+        public TypeOfBonus bonus = TypeOfBonus.None;
     }
 }
